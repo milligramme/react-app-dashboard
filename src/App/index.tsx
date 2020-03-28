15b33df2ba995ebd1aac3cde1cff8a922aee0410
milligramme <milligramme.cc@gmail.com>
2020-03-28 14:11:32 +0900

    replace App

---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./classes.module.css";
+
+const App: React.FunctionComponent = () => (
+  <div className={classes.App}>
+    <p>hello</p>
+  </div>
+);
+
+export default App;
