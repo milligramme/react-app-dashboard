8b2930aa61bff95eb633d64b124e70c572cd0edd
milligramme <milligramme.cc@gmail.com>
2020-03-28 13:30:08 +0900

    add eslint-plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,16 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
+    'react',
     '@typescript-eslint',
+    'react-hooks'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:react-hooks/recommended'
   ],
 };
