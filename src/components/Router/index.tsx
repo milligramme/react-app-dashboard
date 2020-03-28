1e696618cb0fac3a05dc63be6766e0d18c322966
milligramme <milligramme.cc@gmail.com>
2020-03-28 20:47:37 +0900

    set CssBaseline

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { CssBaseline } from "@material-ui/core";
+
 import Home from "../pages/Home";
 import About from "../pages/About";
 
-const Router: React.FunctionComponent = () => {
-
-  return (
+const Router: React.FunctionComponent = () => (
+  <>
+    <CssBaseline />
     <BrowserRouter>
       <Switch>
         <Route exact={true} path="/"><Home /></Route>
         <Route exact={true} path="/about"><About /></Route>
       </Switch>
     </BrowserRouter>
-  );
-};
+  </>
+);
+
 
 export default Router;
