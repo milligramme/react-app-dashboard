8bfc673ac59da0825aa7cd7ff9679707389174ed
milligramme <milligramme.cc@gmail.com>
2020-03-29 21:47:47 +0900

    add notif sample

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import MainTemplate from "../../templates/MainTemplate";
+import { Notif } from "../../organisms/NotificationBar";
 
-const Home: React.FunctionComponent = () => (
-  <MainTemplate>
-    hello
-  </MainTemplate>
-);
+const Home: React.FunctionComponent = () => {
+  const [notif, setNotif] = useState<{ type: Notif, message: string}>();
+
+  // example
+  useEffect(() => {
+    const rand = Math.random() * 10;
+    const randomNotif = rand > 5 ?
+      { type: "error" as Notif, message: `${rand}` } :
+      { type: "info" as Notif, message: `${rand}` };
+
+    setNotif(randomNotif);
+  }, [setNotif]);
+
+  return (
+    <MainTemplate notif={notif}>
+      hello
+    </MainTemplate>
+  );
+}
 
 export default Home;
--- src/components/templates/MainTemplate/index.tsx
+++ src/components/templates/MainTemplate/index.tsx
@@ -4,17 +4,25 @@ import { Container } from "@material-ui/core";
 
 import Header from "../../organisms/Header";
 import Footer from "../../organisms/Footer";
+import NotificationBar, { Notif } from "../../organisms/NotificationBar";
+
 import classes from "./classes.module.css";
 
-const MainTemplate: React.FunctionComponent = ({ children }) => (
+interface OwnProps {
+  notif?: { type: Notif, message: string };
+}
+
+type Props =  OwnProps;
+
+const MainTemplate: React.FunctionComponent<Props> = ({ notif, children }) => (
   <div className={classes.LayoutPage}>
     <Header />
     <Container fixed={true} maxWidth="lg" className={classes.LayoutContainer}>
       {children}
     </Container>
     <Footer />
+    {notif != null && <NotificationBar type={notif.type} message={notif.message} />}
   </div>
 );
-
 
 export default MainTemplate;
