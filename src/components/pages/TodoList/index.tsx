156984eedc105ac994b6501c98e884076a2d1fc5
milligramme <milligramme.cc@gmail.com>
2020-04-04 23:08:49 +0900

    add formik form

--- src/components/pages/TodoList/index.tsx
+++ src/components/pages/TodoList/index.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import { v4 } from "uuid";
+import * as Yup from "yup";
+import { Formik, FormikValues } from "formik";
 
-import { Typography } from "@material-ui/core";
+import { Typography, IconButton } from "@material-ui/core";
+import { AddCircle } from "@material-ui/icons";
+
 import MainTemplate from "../../templates/MainTemplate";
-
+import TextField from "../../atoms/TextField";
+import classes from "./classes.module.css";
 
 interface Task {
   id: string;
@@ -15,13 +21,52 @@ interface LocalState {
 }
 
 const TodoList: React.FunctionComponent = () => {
-  const [tasks, setTask] = useState<LocalState["tasks"]>([]);
+  const [tasks, setTasks] = useState<LocalState["tasks"]>([]);
+
+  const validationSchema = useMemo(() =>
+    Yup.object().shape({
+      id: Yup.string(),
+      title: Yup.string()
+        .required("required !!!"),
+      done: Yup.boolean(),
+    }), []);
 
   return (
     <MainTemplate>
       <Typography>Todo</Typography>
+      <Formik
+        initialValues={{ id: v4(), title: "", done: false }}
+        onSubmit={(values: FormikValues, { resetForm }) => {
+          const task: Task = {
+            id: v4(),
+            title: values.title,
+            done: false
+          };
+
+          setTasks([...tasks, task]);
+          resetForm();
+        }}
+        validationSchema={validationSchema}
+      >
+        {props => (
+          <form onSubmit={props.handleSubmit}>
+            <div className={classes.AddForm}>
+              <TextField label="Add Task" name="title" />
+              <IconButton
+                type="submit"
+                size="small"
+                color="primary"
+                classes={{ root: classes.ButtonSubmit }}
+              >
+                <AddCircle />
+              </IconButton>
+            </div>
+          </form>
+        )}
+      </Formik>
+
       {tasks.map(task =>
-        <div key={task.id}>{task.title}</div>)}
+        <div key={task.id}>{task.id} {task.title}</div>)}
     </MainTemplate>
   )
 }
